eef08f01b05918be98c65be5c85c1c2b8e616328
Anderson Araújo <andersontaraujo92@gmail.com>
2019-01-20 23:07:44 -0300

    feat(app: Added the initial structure for this API.

---
+++ src/main/java/com/accessstop/AccessStopApplication.java
@@ -0,0 +1,14 @@
+package com.accessstop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessStopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessStopApplication.class, args);
+	}
+
+}
+
