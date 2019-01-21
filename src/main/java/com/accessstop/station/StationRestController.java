21a78c85b29b92178ce2787fa06e3c8e84c3d47a
Anderson Targino <andersontargino@cers.com.br>
2019-01-21 10:03:34 -0300

    feat(app): Added GET (by identifier), PUT and DELETE methods for Station service.

--- src/main/java/com/accessstop/station/StationRestController.java
+++ src/main/java/com/accessstop/station/StationRestController.java
@@ -4,8 +4,11 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +32,37 @@ public class StationRestController {
     		Station entity = service.create(station);
     		URI location = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{number}").buildAndExpand(entity.getNumber()).toUri();
     		return ResponseEntity.created(location).body(entity);
+    }
+    
+    @GetMapping("/{number}")
+    public ResponseEntity<Station> findByNumber(@PathVariable("number") Long number) {
+    		Station station = service.findByNumber(number);
+    		if (station == null) {
+    			return ResponseEntity.notFound().build();
+    		}
+    		return ResponseEntity.ok(station);
+    }
+    
+    @PutMapping("/{number}")
+    public ResponseEntity<Station> update(@PathVariable("number") Long number, @RequestBody Station station) {
+    		if (!number.equals(station.getNumber())) {
+    			return ResponseEntity.badRequest().build();
+    		}
+    		Station entity = service.findByNumber(number);
+		if (entity == null) {
+			return ResponseEntity.notFound().build();
+		}
+		entity = service.update(station);
+		return ResponseEntity.ok().body(entity);
+    }
+    
+    @DeleteMapping("/{number}")
+    public ResponseEntity<?> delete(@PathVariable("number") Long number) {
+    		Station entity = service.findByNumber(number);
+		if (entity == null) {
+			return ResponseEntity.notFound().build();
+		}
+		service.delete(number);
+    		return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/accessstop/station/StationService.java
+++ src/main/java/com/accessstop/station/StationService.java
@@ -16,4 +16,17 @@ public class StationService {
     public Station create(Station station) {
     		return repository.save(station);
     }
+    
+    public Station findByNumber(Long number) {
+    		return repository.findByNumber(number);
+    }
+    
+    public Station update(Station station) {
+    		return repository.save(station);
+    }
+    
+    public void delete(Long number) {
+    		repository.deleteById(number);
+    }
+    
 }
